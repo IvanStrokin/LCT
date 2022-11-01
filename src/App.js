618a32b98a1eb59a7d29283220aa3e96428e78bc
Your Name <you@example.com>
2022-11-01 20:27:08 +0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
+import './App.css'
+import Login from "./Components/Login/Login";
+import useToken from "./Components/useToken/useToken";
+import ExcelPage from "./Components/ExcelRender/ExcelRender";
+import "antd/dist/antd.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    const { token, setToken } = useToken();
+    if(!token) {
+        return <Login setToken={setToken} />
+    }
+    return (
+        <div>
+            <ExcelPage/>
+        </div>
+    );
+    }
 export default App;
